c05d10494a730a8963462ab2abe6da715631f210
Arghya Ghosh <71373838+uiuxarghya@users.noreply.github.com>
2024-09-13 07:50:49 +0530

    feat: Add process.c file with code to create a parent-child process

---
+++ Assignment-5/process.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<sys/types.h>
+#include<unistd.h>
+
+int main(){
+	int pid;
+	pid = fork();
+	if (pid > 0){
+		printf("\nPARENT: pid=%d\tppid=%d\n",getpid(),getppid());
+		sleep(9);
+	}
+	if (pid==0){
+		printf("\nCHILD: pid=%d\tppid=%d\n",getpid(),getppid());
+		sleep(9);
+	}
+	return 0;
+}
